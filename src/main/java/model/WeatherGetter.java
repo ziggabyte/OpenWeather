943783bfa0e01b37b0dd73d112d3c1863e185aff
ziggi <ziggi.willpower@gmail.com>
2021-03-30 11:36:32 +0200

    broke down WeatherGetter into smaller methods

--- src/main/java/model/WeatherGetter.java
+++ src/main/java/model/WeatherGetter.java
@@ -18,43 +18,8 @@ import org.xml.sax.InputSource;
 
 public class WeatherGetter {
 
-	public static void getWeather(WeatherBean wBean) throws IOException {
-
-		// Build the API call URL by adding city+country into a URL
-		String URLtoSend = "http://api.openweathermap.org/data/2.5/weather?q=" 
-				+ wBean.getCity() + ","
-				+ wBean.getCountry() + "&APPID=b0b73b21e2a3f1dc048905b0174645a6&mode=xml";
-
-		// Set the URL that will be sent
-		URL line_api_url = new URL(URLtoSend);
-
-		// Create a HTTP connection to sent the GET request over
-		HttpURLConnection linec = (HttpURLConnection) line_api_url.openConnection();
-		linec.setDoInput(true);
-		linec.setDoOutput(true);
-		linec.setRequestMethod("GET");
-
-		// Make a Buffer to read the response from the API
-		BufferedReader in = new BufferedReader(new InputStreamReader(linec.getInputStream()));
-
-		// a String to temp save each line in the response
-		String inputLine;
-
-		// a String to save the full response to use later
-		String ApiResponse = "";
-
-		// loop through the whole response
-		while ((inputLine = in.readLine()) != null) {
-			ApiResponse += inputLine;
-		}
-		
-		in.close();
-
-		// Call a method to make a XMLdoc out of the full response
-		Document doc = convertStringToXMLDocument(ApiResponse);
-
-		// normalize the XML response
-		doc.getDocumentElement().normalize();
+	public static void getWeather(WeatherBean wBean) throws IOException{
+		Document doc = convertStringToXMLDocument(getApiResponse(wBean));
 
 		wBean.setClouds(getXMLAttribute(doc, "clouds", "name"));
 		wBean.setTemperature(getXMLAttribute(doc, "temperature", "value"));
@@ -62,39 +27,62 @@ public class WeatherGetter {
 	}
 	
 	private static String getXMLAttribute(Document doc, String tagName, String attributeName) {
-				NodeList nodeListTagName = doc.getElementsByTagName(tagName);
-				String attribute = null;
+		NodeList nodeListTagName = doc.getElementsByTagName(tagName);
+		String attribute = null;
 				
-				for (int i = 0; i < nodeListTagName.getLength(); i++) {
-					Node node = nodeListTagName.item(i);
-					
-					if (node.getNodeType() == Node.ELEMENT_NODE) {
-						Element eElement = (Element) node;
-						attribute = eElement.getAttribute(attributeName);
-					}
-				}
-		
+		for (int i = 0; i < nodeListTagName.getLength(); i++) {
+			Node node = nodeListTagName.item(i);
+			if (node.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElement = (Element) node;
+				attribute = eElement.getAttribute(attributeName);
+			}
+		}
 		return attribute;
 	}
 
-	// Method the makes a XML doc out of a string, if it is in a XML format.
 	private static Document convertStringToXMLDocument(String xmlString) {
-		// Parser that produces DOM object trees from XML content
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-
-		// API to obtain DOM Document instance
 		DocumentBuilder builder = null;
+		
 		try {
-			// Create DocumentBuilder with default configuration
 			builder = factory.newDocumentBuilder();
-
-			// Parse the content to Document object
 			Document doc = builder.parse(new InputSource(new StringReader(xmlString)));
+			doc.getDocumentElement().normalize();
 			return doc;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	private static String getApiResponse(WeatherBean wBean) throws IOException {
+		HttpURLConnection linec = getHttpConnection(wBean);
+		BufferedReader in = new BufferedReader(new InputStreamReader(linec.getInputStream()));
+
+		String inputLine;
+		String ApiResponse = "";
+
+		while ((inputLine = in.readLine()) != null) {
+			ApiResponse += inputLine;
+		}
+				
+		in.close();
+		return ApiResponse;
+	}
+	
+	private static HttpURLConnection getHttpConnection(WeatherBean wBean) throws IOException {
+		String URLtoSend = "http://api.openweathermap.org/data/2.5/weather?q=" 
+				+ wBean.getCity() + ","
+				+ wBean.getCountry() + "&APPID=b0b73b21e2a3f1dc048905b0174645a6&mode=xml";
+
+		URL line_api_url = new URL(URLtoSend);
+
+		HttpURLConnection linec = (HttpURLConnection) line_api_url.openConnection();
+		linec.setDoInput(true);
+		linec.setDoOutput(true);
+		linec.setRequestMethod("GET");
+		
+		return linec;
 	}
 
 }
